ba6163eebdfc9ff8ac47040eddb401f7daf4adad
bzGong <bzl132@163.com>
2019-01-20 01:22:56 +0800

    model注解插件

---
+++ src/main/java/com/bjiaolong/mybatis/plugins/MapperCommentGenerator.java
@@ -0,0 +1,230 @@
+package com.bjiaolong.mybatis.plugins;
+
+import org.mybatis.generator.api.CommentGenerator;
+import org.mybatis.generator.api.IntrospectedColumn;
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.dom.java.*;
+import org.mybatis.generator.api.dom.xml.TextElement;
+import org.mybatis.generator.api.dom.xml.XmlElement;
+import org.mybatis.generator.config.MergeConstants;
+import org.mybatis.generator.internal.util.StringUtility;
+
+import java.util.Properties;
+import java.util.Set;
+
+/**
+ * @Author: long
+ * @Description:
+ * @Date: Create in 2018.10.13 19:49
+ * @Modified By:
+ * @ClassName: com.bjiaolong.mybatis.plugins.MapperCommentGenerator
+ */
+public class MapperCommentGenerator implements CommentGenerator {
+    //开始的分隔符，例如mysql为`，sqlserver为[
+    private String beginningDelimiter = "";
+    //结束的分隔符，例如mysql为`，sqlserver为]
+    private String endingDelimiter    = "";
+
+    public MapperCommentGenerator() {
+        super();
+    }
+
+    public void addJavaFileComment(CompilationUnit compilationUnit) {
+        return;
+    }
+
+    /**
+     * xml中的注释
+     *
+     * @param xmlElement
+     */
+    public void addComment(XmlElement xmlElement) {
+        xmlElement.addElement(new TextElement("<!--"));
+        StringBuilder sb = new StringBuilder();
+        sb.append("  WARNING - ");
+        sb.append(MergeConstants.NEW_ELEMENT_TAG);
+        xmlElement.addElement(new TextElement(sb.toString()));
+        xmlElement.addElement(new TextElement("-->"));
+    }
+
+    public void addRootComment(XmlElement rootElement) {
+        return;
+    }
+
+    public void addConfigurationProperties(Properties properties) {
+        String beginningDelimiter = properties.getProperty("beginningDelimiter");
+        if (StringUtility.stringHasValue(beginningDelimiter)) {
+            this.beginningDelimiter = beginningDelimiter;
+        }
+        String endingDelimiter = properties.getProperty("endingDelimiter");
+        if (StringUtility.stringHasValue(endingDelimiter)) {
+            this.endingDelimiter = endingDelimiter;
+        }
+    }
+
+    /**
+     * 删除标记
+     *
+     * @param javaElement
+     * @param markAsDoNotDelete
+     */
+    protected void addJavadocTag(JavaElement javaElement, boolean markAsDoNotDelete) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(" * ");
+        sb.append(MergeConstants.NEW_ELEMENT_TAG);
+        if (markAsDoNotDelete) {
+            sb.append(" do_not_delete_during_merge");
+        }
+        javaElement.addJavaDocLine(sb.toString());
+    }
+
+    /**
+     * Example使用
+     *
+     * @param innerClass
+     * @param introspectedTable
+     */
+    public void addClassComment(InnerClass innerClass, IntrospectedTable introspectedTable) {
+    }
+
+    public void addEnumComment(InnerEnum innerEnum, IntrospectedTable introspectedTable) {
+    }
+
+    /**
+     * 给字段添加数据库备注
+     *
+     * @param field
+     * @param introspectedTable
+     * @param introspectedColumn
+     */
+    public void addFieldComment(Field field, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn) {
+        if (StringUtility.stringHasValue(introspectedColumn.getRemarks())) {
+            field.addJavaDocLine("/**");
+            StringBuilder sb = new StringBuilder();
+            sb.append(" * ");
+            sb.append(introspectedColumn.getRemarks());
+            field.addJavaDocLine(sb.toString());
+            field.addJavaDocLine(" */");
+        }
+    }
+
+    /**
+     * Example使用
+     *
+     * @param field
+     * @param introspectedTable
+     */
+    public void addFieldComment(Field field, IntrospectedTable introspectedTable) {
+    }
+
+    public void addModelClassComment(TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+
+    }
+
+    /**
+     * @param method
+     * @param introspectedTable
+     */
+    public void addGeneralMethodComment(Method method, IntrospectedTable introspectedTable) {
+    }
+
+    /**
+     * getter方法注释
+     *
+     * @param method
+     * @param introspectedTable
+     * @param introspectedColumn
+     */
+    public void addGetterComment(Method method, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn) {
+        StringBuilder sb = new StringBuilder();
+        method.addJavaDocLine("/**");
+        if (StringUtility.stringHasValue(introspectedColumn.getRemarks())) {
+            sb.append(" * 获取");
+            sb.append(introspectedColumn.getRemarks());
+            method.addJavaDocLine(sb.toString());
+            method.addJavaDocLine(" *");
+        }
+        sb.setLength(0);
+        sb.append(" * @return ");
+        sb.append(introspectedColumn.getActualColumnName());
+        if (StringUtility.stringHasValue(introspectedColumn.getRemarks())) {
+            sb.append(" - ");
+            sb.append(introspectedColumn.getRemarks());
+        }
+        method.addJavaDocLine(sb.toString());
+        method.addJavaDocLine(" */");
+    }
+
+    /**
+     * setter方法注释
+     *
+     * @param method
+     * @param introspectedTable
+     * @param introspectedColumn
+     */
+    public void addSetterComment(Method method, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn) {
+        StringBuilder sb = new StringBuilder();
+        method.addJavaDocLine("/**");
+        if (StringUtility.stringHasValue(introspectedColumn.getRemarks())) {
+            sb.append(" * 设置");
+            sb.append(introspectedColumn.getRemarks());
+            method.addJavaDocLine(sb.toString());
+            method.addJavaDocLine(" *");
+        }
+        Parameter parm = method.getParameters().get(0);
+        sb.setLength(0);
+        sb.append(" * @param ");
+        sb.append(parm.getName());
+        if (StringUtility.stringHasValue(introspectedColumn.getRemarks())) {
+            sb.append(" ");
+            sb.append(introspectedColumn.getRemarks());
+        }
+        method.addJavaDocLine(sb.toString());
+        method.addJavaDocLine(" */");
+    }
+
+    /**
+     * Example使用
+     *
+     * @param innerClass
+     * @param introspectedTable
+     * @param markAsDoNotDelete
+     */
+    public void addClassComment(InnerClass innerClass, IntrospectedTable introspectedTable, boolean markAsDoNotDelete) {
+    }
+
+    /**
+     * @since mbg 1.3.6
+     */
+    public void addGeneralMethodAnnotation(Method method, IntrospectedTable introspectedTable, Set<FullyQualifiedJavaType> set) {
+
+    }
+
+    /**
+     * @since mbg 1.3.6
+     */
+    public void addGeneralMethodAnnotation(Method method, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn, Set<FullyQualifiedJavaType> set) {
+
+    }
+
+    /**
+     * @since mbg 1.3.6
+     */
+    public void addFieldAnnotation(Field field, IntrospectedTable introspectedTable, Set<FullyQualifiedJavaType> set) {
+
+    }
+
+    /**
+     * @since mbg 1.3.6
+     */
+    public void addFieldAnnotation(Field field, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn, Set<FullyQualifiedJavaType> set) {
+
+    }
+
+    /**
+     * @since mbg 1.3.6
+     */
+    public void addClassAnnotation(InnerClass innerClass, IntrospectedTable introspectedTable, Set<FullyQualifiedJavaType> set) {
+
+    }
+}
